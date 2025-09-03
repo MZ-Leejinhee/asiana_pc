173ff7cad45c072fd8b2c3583bddbc929a579dd5
MZ-Leejinhee <92710726+MZ-Leejinhee@users.noreply.github.com>
2025-09-03 13:42:42 +0900

    Update 2025-09-03T04:42:26.981Z

--- C/pc/js/renew2025.js
+++ C/pc/js/renew2025.js
@@ -243,6 +243,8 @@ function eventSlider2() {
     pagination: '.evt_paging',
     paginationType: 'custom',
     paginationCustomRender: function (swiper, current, total) {
+      if (current < 1) current = 1;
+      
       var formattedCurrent = current < 10 ? '0' + current : current;
       var formattedTotal = total < 10 ? '0' + total : total;
       return '<span class="current">' + formattedCurrent + '</span> / <span class="total">' + formattedTotal + '</span>';
